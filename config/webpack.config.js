37903d3a808a1d9c133359473fb28eaad0d645da
ext.gaomengmeng6 <ext.gaomengmeng6@jd.com>
2023-12-16 17:12:35 +0800

    配置 ghpages static 资源路径

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -202,7 +202,7 @@ module.exports = function (webpackEnv) {
     // This means they will be the "root" imports that are included in JS bundle.
     entry: paths.appIndexJs,
     output: {
-      publicPath: '/',
+     
       library: `base-ui-[name]`,
       libraryTarget: 'umd',
       // The build folder.
@@ -212,17 +212,18 @@ module.exports = function (webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        ? 'react-manage/static/js/[name].[contenthash:8].js'
+        : isEnvDevelopment && 'react-manage/static/js/bundle.js',
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
-      assetModuleFilename: 'static/media/[name].[hash][ext]',
+        ? 'react-manage/static/js/[name].[contenthash:8].chunk.js'
+        : isEnvDevelopment && 'react-manage/static/js/[name].chunk.js',
+      assetModuleFilename: 'react-manage/static/media/[name].[hash][ext]',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
       publicPath: paths.publicUrlOrPath,
+      // publicPath: '/',
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
